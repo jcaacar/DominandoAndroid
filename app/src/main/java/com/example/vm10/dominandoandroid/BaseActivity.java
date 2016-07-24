09788d90a323db3393a92cf86dcba68093173def
jcaac <jcaacar@gmail.com>
2016-07-24 17:24:07 -0300

    removed rect drawing in CustomView increase gradle heap size to 2Gb enable home button in BaseActivity add parentActivity for all Activities except the MainActivity

--- app/src/main/java/com/example/vm10/dominandoandroid/BaseActivity.java
+++ app/src/main/java/com/example/vm10/dominandoandroid/BaseActivity.java
@@ -2,6 +2,7 @@ package com.example.vm10.dominandoandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -13,10 +14,14 @@ public abstract class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
-        if(intent != null && getSupportActionBar() != null) {
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            if(!(this instanceof MainActivity)) actionBar.setDisplayHomeAsUpEnabled(true);
+
             String title = intent.getStringExtra(TITLE_ARG);
-            if(title != null && !title.isEmpty())
-                getSupportActionBar().setTitle(title);
+            if(title != null)
+                actionBar.setTitle(title);
         }
     }
 }
